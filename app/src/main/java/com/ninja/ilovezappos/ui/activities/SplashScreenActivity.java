a3efda70dc85e93ec5524e5c079718c82bfee9c9
Niranjan-B <nero.niranjan@gmail.com>
2017-02-04 20:50:41 -0800

    fading activity transition between splash screen and product screen

--- app/src/main/java/com/ninja/ilovezappos/ui/activities/SplashScreenActivity.java
+++ app/src/main/java/com/ninja/ilovezappos/ui/activities/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.ninja.ilovezappos.ui.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -15,11 +17,24 @@ public class SplashScreenActivity extends AppCompatActivity {
         hideStatusBar();
         setContentView(R.layout.activity_splash_screen);
 
+        fireProductActivity();
     }
 
     private void hideStatusBar() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void fireProductActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent fireProductActivity = new Intent(SplashScreenActivity.this, ProductDisplayActivity.class);
+                startActivity(fireProductActivity);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, 2000);
     }
 }
