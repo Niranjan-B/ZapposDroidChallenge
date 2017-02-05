f7a84f24aaf0088962e328f5864d9b27386f9bd9
Niranjan-B <nero.niranjan@gmail.com>
2017-02-05 12:17:14 -0800

    toolbar containing search bar

--- app/src/main/java/com/ninja/ilovezappos/ui/activities/ProductDisplayActivity.java
+++ app/src/main/java/com/ninja/ilovezappos/ui/activities/ProductDisplayActivity.java
@@ -2,6 +2,8 @@ package com.ninja.ilovezappos.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.ninja.ilovezappos.R;
 
@@ -11,5 +13,15 @@ public class ProductDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_activity_main);
+        TextView mToolbarTitle = (TextView) findViewById(R.id.toolbar_activity_main_text_view);
+
+        setSupportActionBar(mToolbar);
+        mToolbarTitle.setText(getResources().getString(R.string.app_name));
     }
 }
