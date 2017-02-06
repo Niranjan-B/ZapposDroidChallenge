4a67efd1cd0cb33f3884c9f4bee5101f51d42116
Niranjan-B <nero.niranjan@gmail.com>
2017-02-05 21:40:56 -0800

    base views with common methods used across different screens

---
+++ app/src/main/java/com/ninja/ilovezappos/mvp/views/base/ProgressableView.java
@@ -0,0 +1,10 @@
+package com.ninja.ilovezappos.mvp.views.base;
+
+/**
+ * Created by niranjanb on 05/02/17.
+ */
+
+public interface ProgressableView {
+    void hideProgress();
+    void showProgress();
+}
