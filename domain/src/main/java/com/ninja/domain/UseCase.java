4e78d82cfaba9fea0e80e728e41d4df276be048b
Niranjan-B <nero.niranjan@gmail.com>
2017-02-05 21:34:09 -0800

    use case which returns an observable for getting a product based on search parameter

--- domain/src/main/java/com/ninja/domain/UseCase.java
+++ domain/src/main/java/com/ninja/domain/UseCase.java
@@ -1,11 +1,15 @@
 package com.ninja.domain;
 
+import io.reactivex.Observable;
+
 /**
  * Created by niranjanb on 04/02/17.
  */
 
-/**
- * empty stub to compile
- */
-public class UseCase {
+public abstract class UseCase<T> {
+    public abstract Observable<T> buildObservable();
+
+    public Observable<T> execute() {
+        return buildObservable();
+    }
 }
